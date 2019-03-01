d390b0073dd03e25bad14fe67a5136e3bc8d8cd2
trombone761229 <trombone761229@gmail.com>
2019-03-01 08:55:58 +0800

    First commit: First FT and basic Django config

---
+++ lists/tests.py
@@ -0,0 +1,47 @@
+from django.urls import resolve
+from django.test import TestCase
+from lists.views import home_page
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+from lists.models import Item
+# Create your tests here.
+class ItemModelTest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		first_item=Item()
+		first_item.text='The first (ever) list item'
+		first_item.save()
+
+		second_item=Item()
+		second_item.text='Item the second'
+		second_item.save()
+
+		save_items=Item.objects.all()
+		self.assertEqual(save_items.count(),2)
+
+		first_saved_item=save_items[0]
+		second_saved_item=save_items[1]
+		self.assertEqual(first_saved_item.text,'The first (ever) list item')
+		self.assertEqual(second_saved_item.text,'Item the second')
+class HomePageTest(TestCase):
+	def test_root_url_resolves_to_home_page_view(self):
+		found=resolve('/')
+		self.assertEqual(found.func,home_page)
+	def test_home_page_return_correct_html(self):
+		request=HttpRequest()
+		response=home_page(request)
+		self.assertIn('A new list item',response.content.decode())
+		expected_html=render_to_string('home.html',
+			{'new_item_text':'A new list item'})
+		self.assertEqual(response.content.decode(),expected_html)
+		#html=response.content.decode('utf8')
+		#self.assertTrue(html.startswith('<html>'))
+		#self.assertIn('<title>To-Do lists</title>',html)
+		#self.assertTrue(html.strip().endswith('</html>'))
+	def test_home_page_can_save_a_POST_request(self):
+		response=self.client.post('/',data={'item_text':'A new list item'})
+		self.assertEqual(Item.objects.count(),1)
+		new_item=Item.objects.first()
+		self.assertEqual(new_item.text,'A new list item')
+		
+		self.assertIn('A new list item',response.content.decode())
+		self.assertTempliatUsed(response,'home.html')
---
+++ superlists/functional_tests.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+import time
+class NewVistorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser=webdriver.Firefox()
+		self.browser.implicitly_wait(5)
+	def tearDown(self):
+		#self.browser.quit()
+		pass
+	def check_for_row_in_list_table(self,row_text):
+		table=self.browser.find_element_by_id('id_list_table')
+		rows=table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text,[row.text for row in rows])
+	
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://localhost:8000')
+		self.assertIn('To-Do',self.browser.title)
+		header_text=self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do',header_text)
+		inputbox=self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+		time.sleep(5)
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(5)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+		inputbox=self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(5)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.fail('Finish the test!')
+if __name__=='__main__':
+	unittest.main(warnings='ignore')
