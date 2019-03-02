3b41767ed9b53d0b78a70d41af0dc6cd221c55aa
trombone761229 <trombone761229@gmail.com>
2019-03-02 22:04:44 +0800

    Use Bootstrap to improve layout

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,94 @@
-from django.urls import resolve
 from django.test import TestCase
-from lists.views import home_page
-from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item,List
 # Create your tests here.
+class ListAndItemModelsTest(TestCase):
+	def test_saving_andretrieving_items(self):
+		list_=List()
+		list_.save()
+
+		first_item=Item()
+		first_item.text='The first (ever) list item'
+		first_item.list=list_
+		first_item.save()
+
+		second_item=Item()
+		second_item.text='Item the second'
+		second_item.list=list_
+		second_item.save()
+
+		saved_list=List.objects.first()
+		self.assertEqual(saved_list,list_)
+
+		saved_items=Item.objects.all()
+		self.assertEqual(saved_list,list_)
+
+		saved_items=Item.objects.all()
+		self.assertEqual(saved_items.count(),2)
+
+		first_saved_item=saved_items[0]
+		second_saved_item=saved_items[1]
+		self.assertEqual(first_saved_item.text,'The first (ever) list item')
+		self.assertEqual(first_saved_item.list,list_)
+		self.assertEqual(second_saved_item.text,'Item the second')
+		self.assertEqual(second_saved_item.list,list_)
+class NewListTest(TestCase):
+	def test_save_a_POST_request(self):
+		self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+	def test_redirects_after_POST(self):
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item' } )
+		new_list=List.objects.first()
+		self.assertRedirects(response,'/lists/%d/' %(new_list.id))
+		self.assertEqual(response.status_code, 302)
+class NewItemTest(TestCase):
+	def test_can_save_a_POST_request_to_an_existing_list(self):
+		other_list=List.objects.create()
+		correct_list=List.objects.create()
+
+		self.client.post('/lists/%d/add_item' % (correct_list.id,),data={'item_text':'A new item for an existing list'})
+
+		self.assertEqual(Item.objects.count(),1)
+		new_item=Item.objects.first()
+		self.assertEqual(new_item.text,'A new item for an existing list')
+		self.assertEqual(new_item.list,correct_list)
+
+	def test_redirects_to_list_view(self):
+		other_list=List.objects.create()
+		correct_list=List.objects.create()
+
+		response=self.client.post('/lists/%d/add_item' % (correct_list.id,),data={'item_text':'A new item for an existing list'})
+
+		self.assertRedirects(response,'/lists/%d/' % (correct_list.id,))
+
+class ListViewTest(TestCase):
+	def test_displays_only_items_for_that_list(self):
+		correct_list=List.objects.create()
+		Item.objects.create(text='tiemey 1',list=correct_list)
+		Item.objects.create(text='itemey 2',list=correct_list)
+		
+		other_list=List.objects.create()
+		Item.objects.create(text='other list item 1',list=other_list)
+		Item.objects.create(text='other list item 2',list=other_list)
+	
+		response=self.client.get('/lists/%d/' %(correct_list.id,) )
+		
+		self.assertContains(response,'itemey 1')
+		self.assertContains(response,'itemey 2')
+		self.assertNotContains(response,'other list item 1')
+		self.assertNotContains(response,'other list item 2')
+	def test_uses_list_template(self):
+		list_=List.objects.create()
+		response=self.client.get('/lists/%d/' %(list_.id,))
+		self.assertTemplateUsed(response,'list.html')
+	def test_passes_correct_list_to_template(self):
+		other_list=List.objects.create()
+		correct_list=List.objects.create()
+		response=self.client.get('/lists/%d/' % (correct_list.id,))
+		self.assertEqual(response.context['list'],correct_list)
 class ItemModelTest(TestCase):
 	def test_saving_and_retrieving_items(self):
 		first_item=Item()
@@ -23,9 +107,6 @@ class ItemModelTest(TestCase):
 		self.assertEqual(first_saved_item.text,'The first (ever) list item')
 		self.assertEqual(second_saved_item.text,'Item the second')
 class HomePageTest(TestCase):
-	def test_root_url_resolves_to_home_page_view(self):
-		found=resolve('/')
-		self.assertEqual(found.func,home_page)
 	def test_home_page_return_correct_html(self):
 		request=HttpRequest()
 		response=home_page(request)
@@ -37,11 +118,5 @@ class HomePageTest(TestCase):
 		#self.assertTrue(html.startswith('<html>'))
 		#self.assertIn('<title>To-Do lists</title>',html)
 		#self.assertTrue(html.strip().endswith('</html>'))
-	def test_home_page_can_save_a_POST_request(self):
-		response=self.client.post('/',data={'item_text':'A new list item'})
-		self.assertEqual(Item.objects.count(),1)
-		new_item=Item.objects.first()
-		self.assertEqual(new_item.text,'A new list item')
-		
-		self.assertIn('A new list item',response.content.decode())
-		self.assertTempliatUsed(response,'home.html')
+
+
--- superlists/urls.py
+++ superlists/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from django.conf.urls import include,url
 from lists import views
 from lists.views import home_page
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 """
 urlpatterns = [
     path('admin/', admin.site.urls),
